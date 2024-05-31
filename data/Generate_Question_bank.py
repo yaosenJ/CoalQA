defad47addf2767011418bc337b2334a5264d58d
江耀森 <147613954+yaosenJ@users.noreply.github.com>
2024-05-31 12:39:20 +0800

    Add files via upload

---
+++ data/Generate_Question_bank.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+import csv
+import json
+
+# 假设CSV文件的名称是'questions.csv'，且无标题行
+csv_filename = r'C:\Users\jys\OneDrive\桌面\data\多选题.csv'
+json_filename = '煤矿安全数据/多项选择题.json'
+
+# 读取CSV文件并转换为JSON格式
+conversations_list = []
+with open(csv_filename, mode='r', encoding='gbk') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    for row in csv_reader:
+        question, answer = row
+        conversation = {
+            "system": "你是一个煤矿安全领域的知识达人，你会解答很多题。用户给出一个多选题，你能从几个选项中，选择出多个正确选项。",
+            "input": question,
+            "output": answer
+        }
+        conversations_list.append({"conversation": [conversation]})
+
+# 创建最终的JSON结构
+json_data = conversations_list
+
+# 写入JSON文件
+with open(json_filename, mode='w', encoding='utf-8') as jsonfile:
+    json.dump(json_data, jsonfile, ensure_ascii=False, indent=4)
+
+print(f"转换完成，JSON文件已保存为 '{json_filename}'")
---
+++ data/format_internlm2.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+import json
+
+# 假设JSON文件的名称是'questions.json'
+input_json_filename = './data/output/data.json'
+output_json_filename = '煤矿安全数据/安全规程制度.json'
+
+# 读取原始JSON文件
+with open(input_json_filename, 'r', encoding='utf-8') as infile:
+    data = json.load(infile)
+
+# 转换数据格式
+converted_data = [
+    {"conversation": [
+        {
+            "system": "你是一个煤矿安全领域的知识达人，你对相关煤矿安全规章规程制度、技术等文档非常熟悉。请你专业正确地解答用户想问的煤矿安全相关问题。",
+            "input": item["input"],
+            "output": item["output"]
+        }
+    ]} for item in data
+]
+
+# 写入转换后的JSON文件
+with open(output_json_filename, 'w', encoding='utf-8') as outfile:
+    json.dump(converted_data, outfile, ensure_ascii=False, indent=4)
+
+print(f"转换完成，JSON文件已保存为 '{output_json_filename}'")
